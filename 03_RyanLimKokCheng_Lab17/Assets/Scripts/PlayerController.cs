5e07db2e1b9476672aa169ac8bc65deea05af1c9
SumanaiEugen <48399145+SumanaiEugen@users.noreply.github.com>
2021-01-20 16:43:13 +0800

    Main

---
+++ 03_RyanLimKokCheng_Lab17/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    float Speed = 5;
+    float jumpForce = 10;
+
+    int HealthCount = 100;
+    int coinCount = 0;
+
+    public Text HealthText;
+    public Text Cointext;
+
+    Rigidbody2D rb;
+    private Animator animator;
+    private AudioSource audiosoure;
+
+    public AudioClip[] audioClipArray;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        HealthText.GetComponent<Text>().text = "Health : " + HealthCount;
+        Cointext.GetComponent<Text>().text = "Coin : " + coinCount;
+
+        audiosoure = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            HealthCount -= 10;
+            HealthText.GetComponent<Text>().text = "Health : " + HealthCount;
+
+            int rand = Random.Range(1, 2);
+
+            audiosoure.PlayOneShot(audioClipArray[rand]);
+        }
+        if (collision.gameObject.tag == "Coin")
+        {
+            coinCount += 1;
+            Destroy(collision.gameObject);
+            Cointext.GetComponent<Text>().text = "Coin : " + coinCount;
+
+            audiosoure.PlayOneShot(audioClipArray[0]);
+        }
+    }
+
+    private void Movement()
+    {
+        float hVelocity = 0;
+        float vVelocity = 0;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            hVelocity = -Speed;
+            transform.localScale = new Vector3(-1, 1, 1);
+            animator.SetFloat("xVelocity", Mathf.Abs(hVelocity));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            hVelocity = Speed;
+            transform.localScale = new Vector3(1, 1, 1);
+            animator.SetFloat("xVelocity", Mathf.Abs(hVelocity));
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            vVelocity = jumpForce;
+            animator.SetTrigger("Jumptrigger");
+
+            audiosoure.PlayOneShot(audioClipArray[3]);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            animator.SetFloat("xVelocity", 0);
+        }
+
+        hVelocity = Mathf.Clamp(rb.velocity.x + hVelocity, -5, 5);
+        rb.velocity = new Vector2(hVelocity, rb.velocity.y + vVelocity);
+    }
+}
